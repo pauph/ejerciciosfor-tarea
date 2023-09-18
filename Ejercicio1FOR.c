9b1b2c5ff53ddf5edb7341d332bf3795d8e42474
Ana Paula Percino Huerta <128005733+pauph@users.noreply.github.com>
2023-09-17 19:12:51 -0600

    Create Ejercicio1FOR.c

---
+++ Ejercicio1FOR.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    int cont,num;
+	printf("Cuantos numeros quieres que imprima: ");
+	scanf("%d", &num);
+	for (cont=1;cont<=num;cont=cont){
+		printf("%d",cont);
+		printf("-%d ",cont);
+		cont++;
+	}
+    return 0;
+}
